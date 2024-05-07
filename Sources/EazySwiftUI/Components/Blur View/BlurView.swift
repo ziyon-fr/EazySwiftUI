de3aab7bdea1931edad2ee9c3d1532427c71991d
Leon Salvatore <leon.fernandes@ziyon.fr>
2024-05-07 20:56:59 +0200

    [iOS]-[Components] - Blur View V1

--- Sources/EazySwiftUI/Components/Blur View/BlurView.swift
+++ Sources/EazySwiftUI/Components/Blur View/BlurView.swift
@@ -1,6 +1,6 @@
 //
 //  BlurView.swift
-//  
+//
 //
 //  Created by Elioene Silves Fernandes on 07/05/2024.
 //
@@ -11,7 +11,7 @@ import SwiftUI
 public struct BlurView: UIViewRepresentable {
     /// The style of the blur effect.
     var style: UIBlurEffect.Style
-
+    
     /// Creates the `UIVisualEffectView` with the specified blur effect style.
     ///
     /// - Parameter context: The context for coordinating with the SwiftUI runtime.
@@ -20,14 +20,14 @@ public struct BlurView: UIViewRepresentable {
         let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
         return view
     }
-
+    
     /// Updates the `UIVisualEffectView` when the SwiftUI view hierarchy changes.
     ///
     /// - Parameters:
     ///   - uiView: The view being updated.
     ///   - context: The context for coordinating with the SwiftUI runtime.
     public func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-
+        
     }
 }
 
@@ -39,7 +39,6 @@ struct BlurViewPreview: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                                
                 VStack(spacing: defaultPadding) {
                     
                     Circle()
@@ -55,9 +54,7 @@ struct BlurViewPreview: View {
                             .frame(height: 100)
                     }
                 }
-                .padding()
-                
-                BlurView(style: .systemUltraThinMaterialLight)
+                BlurView(style: .systemChromeMaterialLight)
                     .ignoresSafeArea()
             }
             .navigationTitle("Blur View")
